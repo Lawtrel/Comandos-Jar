b0b139f7c203adbd887a9d2603a84d5859c50d8b
Kazemarus <capcom_x9@hotmail.com>
2020-01-20 12:00:31 -0300

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+	public static void main(String[] args) {
+	/*
+	System.out.println("AADSA Java");
+	int jogo = 15;
+	System.out.println("o jogo custa R$" +jogo);
+	*/
+	/*Tipos de Variaveis
+	int numero = 19;
+	String frase = "olá mundo";
+	boolean var = true;
+	char var2 = 's';
+	double var3 = 24.9;
+	System.out.println(numero);
+	System.out.println(frase);
+	System.out.println(var);
+	System.out.println(var2);
+	System.out.println(var3);
+	*/
+	/*Constants
+		final int  vida_maxima = 10;
+		System.out.println(vida_maxima);
+	*/
+	/*Arrays
+	String[] nome = new String[5];
+	nome[0] = "Mac";
+	nome[1] = "Linux";
+	nome[2] = "Windows";
+	System.out.println(nome[0]);
+	*/
+	}
+}
