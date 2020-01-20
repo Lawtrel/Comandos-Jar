5c222e0a697e4ba339374276e577ce9d4d928593
Kazemarus <capcom_x9@hotmail.com>
2020-01-20 13:41:00 -0300

    3

--- src/Main.java
+++ src/Main.java
@@ -1,72 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-	/*
-	System.out.println("AADSA Java");
-	int jogo = 15;
-	System.out.println("o jogo custa R$" +jogo);
-	*/
-	/*Tipos de Variaveis
-	int numero = 19;
-	String frase = "olá mundo";
-	boolean var = true;
-	char var2 = 's';
-	double var3 = 24.9;
-	System.out.println(numero);
-	System.out.println(frase);
-	System.out.println(var);
-	System.out.println(var2);
-	System.out.println(var3);
-	*/
-	/*Constants
-		final int  vida_maxima = 10;
-		System.out.println(vida_maxima);
-	*/
-	/*Arrays
-	String[] nome = new String[5];
-	nome[0] = "Mac";
-	nome[1] = "Linux";
-	nome[2] = "Windows";
-	System.out.println(nome[0]);
-	*/
-	/*Condições
-	int vida = 101;
-	if(vida >= 100){
-		vida+=100; // condicao de adicao
-		System.out.println("Minha vida bosta é "+vida);
-	}else{
-		System.out.println("Minha vida bosta é "+vida);
-	}
-	//CASES
-		int vida = 50;
-		switch(vida)
-		{
-		case 90:
-		//acao
-		System.out.println("Super");
-		break;
-		case 30:
-		System.out.println("Hyper");
-		break;
-		default:
-		System.out.println("TrapStar");
-		break;
-		}
-		* Operacao
-		int vida1 = 100;
-		int vida2 = 50;
-		int vidatotal = vida1 + vida2 +150;
-		System.out.println((vidatotal) /2 * 3 ) ;
-		*
-		int contador = 1;
-		while(contador <= 10) {
-			System.out.println(contador);
-			contador++;}
-		*/
-		for(int i= 1;i<=10;i++) {
-			System.out.println(+i);
-		}
-	
-	
+		Scanner in = new Scanner(System.in);
+		String nome,idade,peso,cidade,pais;
+		System.out.println("Name plz: ");
+		nome = in.nextLine();
 	}
 }
